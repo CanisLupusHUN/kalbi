b9c1952996b80df347818eba334789481642d7e6
Cseh Viktor <cseh.viktor@kszk.bme.hu>
2022-08-16 19:13:09 +0200

    sip/message/NewResponse fails in the example code fix

--- sip/message/response.go
+++ sip/message/response.go
@@ -1,10 +1,10 @@
 package message
 
 // NewResponse creates new SIP Response
-func NewResponse(request *SipReq, via *SipVia, to *SipTo, from *SipFrom, callID *SipVal, maxfor *SipVal) *SipMsg {
+func NewResponse(request *SipReq, via *[]SipVia, to *SipTo, from *SipFrom, callID *SipVal, maxfor *SipVal) *SipMsg {
 	r := new(SipMsg)
 	r.Req = *request
-	r.Via = append(r.Via, *via)
+	r.Via = *via
 	r.To = *to
 	r.From = *from
 	r.CallID = *callID
