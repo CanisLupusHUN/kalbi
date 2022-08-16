551d87eb1e34b8873d3c07f6af5b78df84788cd4
Cseh Viktor <cseh.viktor@kszk.bme.hu>
2022-08-16 20:27:32 +0200

    change from and to in response

--- sip/message/response.go
+++ sip/message/response.go
@@ -5,8 +5,26 @@ func NewResponse(request SipReq, via []SipVia, to SipTo, from SipFrom, callID Si
 	r := new(SipMsg)
 	r.Req = request
 	r.Via = via
-	r.To = to
-	r.From = from
+	r.To = SipTo{
+		UriType:  from.UriType,
+		Name:     from.Name,
+		User:     from.User,
+		Host:     from.Host,
+		Port:     from.Port,
+		Tag:      from.Tag,
+		UserType: from.UserType,
+		Src:      from.Src,
+	}
+	r.From = SipFrom{
+		UriType:  to.UriType,
+		Name:     to.Name,
+		User:     to.User,
+		Host:     to.Host,
+		Port:     to.Port,
+		Tag:      to.Tag,
+		UserType: to.UserType,
+		Src:      to.Src,
+	}
 	r.CallID = callID
 	r.MaxFwd = maxfor
 	return r
